5de22bcdbbb501b37b26749f83106ce4cc4ca3c6
ys60123in <ys60123in@gmail.com>
2020-10-11 19:53:15 +0800

    BMI計算器更新
    1.新增刪除功能
    2.重置按鈕不整個頁面Refresh
    3.新增BMI狀態管理
    4.提示訊息改成顯示在畫面上
    5.計算式修正

--- FinalHW2_BMICalculator/js/all.js
+++ FinalHW2_BMICalculator/js/all.js
@@ -1,137 +1,145 @@
-//Parameter
-let resultBtn = document.getElementById('btnResult');
-let refreshBtn = document.querySelector('.refresh');
-let historyArray = JSON.parse(localStorage.getItem('historyArray')) || [];
+const BMILevel = {
+    'under-weight':{
+        desc: '過輕',
+        color: '#31BAF9'
+    },
+    'healthy-weight':{
+        desc: '理想',
+        color: '#86D73F'
+    },
+    'over-weight':{
+        desc: '過重',
+        color: '#FF982D'
+    },
+    'obese-class1':{
+        desc: '輕度肥胖',
+        color: '#FF6C03'
+    },
+    'obese-class2':{
+        desc: '中度肥胖',
+        color: '#FF6C03'
+    },
+    'obese-class3':{
+        desc: '重度肥胖',
+        color: '#FF1200'
+    }
+};
 
+//Parameter
+const resultBtn = document.getElementById('btnResult');
+const refreshBtn = document.querySelector('.refresh');
+let historyArray = JSON.parse(localStorage.getItem('historyArray')) || [];
+const list = document.querySelector('.list');
+const promptHeight = document.getElementById('promptHeight');
+const promptWeight = document.getElementById('promptWeight');
+const textHeight = document.getElementById('height');
+const textWeight = document.getElementById('weight');
+
+//Main
 updateHistory();
 
 //Event
 resultBtn.addEventListener('click', calculateBMI, false);
 refreshBtn.addEventListener('click', refresh, false);
+list.addEventListener('click', removeBMIHistory, false);
+textHeight.addEventListener('blur', showPrompt, false);
+textWeight.addEventListener('blur', showPrompt, false);
 
 //Function
 function calculateBMI(){
-    let textHeight = document.getElementById('height');
-    let textWeight = document.getElementById('weight');
-    let height, weight, BMI, level, levelStyle;
+    let height, weight, BMI, level;
 
-    if(textHeight.value == '' || isNaN(textHeight.value))
+    if(textHeight.value === '' || isNaN(textHeight.value))
     {
-        alert("身高(cm)未輸入或輸入值並非數字!!");
+        promptHeight.textContent = "身高(cm)未輸入或輸入值並非數字!!";
         return;
     }
-    if(textWeight.value == '' || isNaN(textWeight.value))
+    if(textWeight.value === '' || isNaN(textWeight.value))
     {
-        alert("體重(kg)未輸入或輸入值並非數字!!");
+        promptWeight.textContent = "體重(kg)未輸入或輸入值並非數字!!";
         return;
     }
     height = parseFloat(textHeight.value);
     if(height <= 0)
     {
-        alert("身高(cm)必須大於0");
+        promptHeight.textContent = "身高(cm)必須大於0";
         return;
     }
     weight = parseFloat(textWeight.value);
     if(weight <= 0)
     {
-        alert("體重(kg)必須大於0");
+        promptWeight.textContent = "體重(kg)必須大於0";
         return;
     }
-    BMI = (weight / ((height / 100) ^ 2)).toFixed(2);
+    promptHeight.textContent = "";
+    promptWeight.textContent = "";
+    BMI = (weight / ((height / 100) * (height / 100))).toFixed(2);
     console.log(BMI);
 
     if(BMI < 18.5)
     {
-        level = '過輕';
-        levelStyle = 'under-weight';
+        level = 'under-weight';
     }
     else if(BMI >= 18.5 && BMI < 25)
     {
-        level = '理想';
-        levelStyle = 'healthy-weight';
+        level = 'healthy-weight';
     }
     else if(BMI >= 25 && BMI < 30)
     {
-        level = '過重';
-        levelStyle = 'over-weight';
+        level = 'over-weight';
     }
     else if(BMI >= 30 && BMI < 35)
     {
-        level = '輕度肥胖';
-        levelStyle = 'obese-class1';
+        level = 'obese-class1';
     }
     else if(BMI >= 35 && BMI < 40)
     {
-        level = '中度肥胖';
-        levelStyle = 'obese-class2';
+        level = 'obese-class2';
     }
     else if(BMI >= 40)
     {
-        level = '重度肥胖';
-        levelStyle = 'obese-class3';
+        level = 'obese-class3';
     }
 
-    showResult(BMI, level, levelStyle);
-    saveLocalStorage(height, weight, BMI, level, levelStyle);
+    showResult(BMI, level);
+    saveLocalStorage(height, weight, BMI, level);
 }
 
-function showResult(BMI, level, levelStyle){
-    let labelBMI = document.querySelector('.label-BMI');
-    let resultLevel = document.querySelector('.result-level');
-    let color = '';
+function showResult(BMI, level){
+    const labelBMI = document.querySelector('.label-BMI');
+    const resultLevel = document.querySelector('.result-level');
 
-    switch(levelStyle)
-    {
-        case 'under-weight':
-            color = '#31BAF9';
-            break;
-        
-        case 'healthy-weight':
-            color = '#86D73F';
-            break;
-
-        case 'over-weight':
-            color = '#FF982D';
-            break;
-
-        case 'obese-class1':
-            color = '#FF6C03';
-            break;
-
-        case 'obese-class2':
-            color = '#FF6C03';
-            break;
-        
-        case 'obese-class3':
-            color = '#FF1200';
-            break;
-        
-        default:
-            color = '#ffffff';
-            break;
-    }
-
-    labelBMI.setAttribute('style', 'display: block;color: ' + color);
-    resultLevel.setAttribute('style', 'display: block;color: ' + color);
-    resultLevel.textContent = level;
-    refreshBtn.setAttribute('style', 'display: block;background-color: ' + color);
-    resultBtn.setAttribute('style', 'cursor: default;pointer-events: none;background: transparent;font-size: 32px;color: ' + color + ';border: 6px solid ' + color);
+    labelBMI.setAttribute('style', `display: block;color:${BMILevel[level].color}`);
+    resultLevel.setAttribute('style', `display: block;color:${BMILevel[level].color}`);
+    resultLevel.textContent = BMILevel[level].desc;
+    refreshBtn.setAttribute('style', `display: block;background-color:${BMILevel[level].color}`);
+    resultBtn.setAttribute('style', `cursor: default;pointer-events: none;background: transparent;font-size: 32px;color:${BMILevel[level].color};border: 6px solid ${BMILevel[level].color}`);
     resultBtn.value = BMI;
 }
 
 function refresh(){
-    window.location.reload();
+    const labelBMI = document.querySelector('.label-BMI');
+    const resultLevel = document.querySelector('.result-level');
+
+    labelBMI.setAttribute('style', 'display: none;');
+    resultLevel.setAttribute('style', 'display: none;');
+    refreshBtn.setAttribute('style', 'display: none;');
+    resultBtn.setAttribute('style', 'cursor: pointer;pointer-events: auto;background: #FFD366;font-size: 24px;color: #424242;border: 6px solid transparent');
+    resultBtn.value = "看結果";
+    textHeight.value = "";
+    textWeight.value = "";
 }
 
-function saveLocalStorage(height, weight, BMI, level, levelStyle){
+function saveLocalStorage(height, weight, BMI, level){
     let today = new Date();
     let date = paddingLeft(String(today.getMonth() + 1), 2) + '-' + paddingLeft(String(today.getDate()), 2) + '-' + today.getFullYear();
     let data = {
+        Id: Math.floor(Date.now()),
         Height: height,
         Weight: weight,
         BMI: BMI,
-        Level: level,
-        LevelStyle: levelStyle,
+        Level: BMILevel[level].desc,
+        LevelStyle: level,
         Date: date
     };
     //只保留10筆歷史紀錄
@@ -146,7 +154,7 @@ function saveLocalStorage(height, weight, BMI, level, levelStyle){
 }
 
 function updateHistory(){
-    let list = document.querySelector('.list');
+    const list = document.querySelector('.list');
     let str = '';
     for (let i = 0; i < historyArray.length; i++) {
         str += 
@@ -156,6 +164,7 @@ function updateHistory(){
             <p class="item-weight"><span class="desc">weight</span>${historyArray[i].Weight}kg</p>
             <p class="item-height"><span class="desc">height</span>${historyArray[i].Height}cm</p>
             <p class="item-date desc">${historyArray[i].Date}</p>
+            <a class="item-remove" href="#"><img class="btn-remove" key="${historyArray[i].Id}" src="https://image.flaticon.com/icons/png/512/484/484662.png" alt=""></a>
         </li>`;
     }
     list.innerHTML = str;
@@ -169,5 +178,58 @@ function paddingLeft(str, length){
     else
     {
         return paddingLeft('0' + str, length);
+    }
+}
+
+function removeBMIHistory(e){
+    e.preventDefault();
+    if(e.target.nodeName !== 'IMG') return;
+    let index = historyArray.findIndex(i => i.Id === Number(e.target.getAttribute('key')));
+    // console.log(index);
+    if(index >= 0)
+    {
+        historyArray.splice(index, 1);
+        localStorage.setItem('historyArray', JSON.stringify(historyArray));
+        updateHistory();
+    }
+}
+
+function showPrompt(e){
+    let height, weight;
+    //提示身高輸入是否正確
+    if(e.target.getAttribute('name') === 'height')
+    {
+        if(e.target.value === '' || isNaN(e.target.value))
+        {
+            promptHeight.textContent = "身高(cm)未輸入或輸入值並非數字!!";
+            return;
+        }
+
+        height = parseFloat(e.target.value);
+        if(height <= 0)
+        {
+            promptHeight.textContent = "身高(cm)必須大於0";
+            return;
+        }
+
+        promptHeight.textContent = "";
+    }
+    //提示體重輸入是否正確
+    if(e.target.getAttribute('name') === 'weight')
+    {
+        if(e.target.value === '' || isNaN(e.target.value))
+        {
+            promptWeight.textContent = "體重(kg)未輸入或輸入值並非數字!!";
+            return;
+        }
+        
+        weight = parseFloat(e.target.value);
+        if(weight <= 0)
+        {
+            promptWeight.textContent = "體重(kg)必須大於0";
+            return;
+        }
+
+        promptWeight.textContent = "";
     }
 }
